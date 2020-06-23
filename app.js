3844524608d67a08308fd2cf5742bedb45e39d5d
Tran Manh Linh <devngmo@gmail.com>
2020-06-23 14:05:09 +0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,21 @@
+const app = new Vue({
+	el: '#app',
+	data: {
+		
+	},
+	mounted : function() {
+
+	},
+	created: function() {
+		console.log('[Todolist] created');
+	},
+	watch: {
+		
+	},
+	computed: {
+		
+	},
+	methods: {
+		
+	}
+});
